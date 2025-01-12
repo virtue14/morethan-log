cb70c8b254aeb5ccd79d5df1de363bbbc133152d
악덕 <rudejrqkr97@gmail.com>
2025-01-13 00:39:59 +0900

    refactor: 프로필 및 블로그 정보 업데이트
    - site.config.js 파일에서 프로필 및 블로그 정보를 수정하였습니다.
    - 이름, 역할, 이메일, 깃허브 계정 등의 프로필 정보를 변경하였습니다.
    - 블로그의 제목, 설명, 테마 설정을 업데이트하였습니다.
    - 언어 설정을 'ko-KR'로 변경하였습니다.

--- site.config.js
+++ site.config.js
@@ -1,32 +1,32 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "악덕",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Backend Developer",
+    bio: "악덕의 기술 블로그",
+    email: "gdpark.dev@gmail.com",
+    linkedin: "",
+    github: "virtue14",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "virtue-log",
+    description: "welcome to virtue-log!",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
