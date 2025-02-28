182276f665ba57574573d543c52a6e2c0c5d1c8c
박경덕 <gdpark@leadermine.co.kr>
2025-02-28 18:00:20 +0900

    refactor(next.config.js): rewrites 함수명을 redirects로 변경
    - API 경로 리다이렉션을 명확히 하기 위한 함수명 변경
    - 주석 추가로 브라우저 캐싱 방지 목적 설명

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ["www.notion.so", "lh5.googleusercontent.com", "s3-us-west-2.amazonaws.com"],
   },
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: "/api/:path*",
-        has: [{ type: "protocol", value: "https" }],
         destination: "http://api.virtue-world.info:8081/:path*",
-        permanent: false,
+        permanent: false, // HTTP 유지 (브라우저 캐싱 방지)
       },
     ];
   },
