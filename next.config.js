9afda534d9045274424093d6936f1fd93c7baa9f
박경덕 <gdpark@leadermine.co.kr>
2025-02-28 13:16:46 +0900

    feat: API 경로 리다이렉션 추가
    - API 요청을 외부 서버로 리다이렉션 설정
    - /api/:path* 경로를 http://api.virtue-world.info/:path*로 리다이렉션
    - 외부 API 통합으로 데이터 처리 효율성 증대

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://api.virtue-world.info/:path*",
+      },
+    ];
+  },
+};
